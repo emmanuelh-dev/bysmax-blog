1f1c62be9871c9e7dac02c0e88b62f63c9d164cc
Emmanuel Hernandez <e805177@gmail.com>
2024-03-08 01:31:11 -0600

    Se mejora la imagen de marca,

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,40 +10,37 @@ const editUrl = `${siteMetadata.siteRepo}/new/main/data/blog`
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
+    <div>
+      <header className="flex items-center justify-between py-10">
+        <div>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
             </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
-    </header>
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+              >
+                {link.title}
+              </Link>
+            ))}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+    </div>
   )
 }
 
