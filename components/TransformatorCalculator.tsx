8d168900116d966a8d03d63930ba8ae9470975e5
Emmanuel Hernandez <e805177@gmail.com>
2024-03-09 17:44:07 -0600

    Fix: imports

--- components/TransformatorCalculator.tsx
+++ components/TransformatorCalculator.tsx
@@ -1,8 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import { Label } from './ui/label'
-import { Input } from './ui/Input'
-import { Button } from './ui/button'
+import { Label } from '../components/ui/label'
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
 
 const TransformatorCalculator = () => {
   const [voltageIn, setVoltageIn] = useState(220)
@@ -38,64 +39,75 @@ const TransformatorCalculator = () => {
     return Math.sqrt(2) * efective
   }
   return (
-    <div>
-      <div className="mb-3">
-        <Label htmlFor="voltageout">Voltaje de salida</Label>
-        <Input type="number" onChange={handleChangeOut} placeholder="0" />
-      </div>
+    <div className="flex gap-4 max-sm:flex-col">
       <div>
-        <Label htmlFor="voltagein">Voltaje de entrada</Label>
-        <Input type="number" onChange={handleChangeIn} placeholder="0" />
-        <Button type="submit" value={'Calcular'}>
-          Calcular
-        </Button>
+        <Card>
+          <CardContent>
+            <CardTitle>Calculadora</CardTitle>
+            <Input type="number" onChange={handleChangeOut} placeholder="0" />
+            <Label htmlFor="voltageout">Voltaje de salida</Label>
+
+            <Label htmlFor="voltagein">
+              Voltaje de entrada
+              <Input type="number" onChange={handleChangeIn} placeholder="0" />
+            </Label>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" value="Calcular" className="w-full">
+              Calcular
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
 
-      <h2 className="py-3 text-3xl font-bold">Subtitulo</h2>
-      <h3>
-        <span className="font-bold">
-          Impedancia del bobinado primario: <br />
-        </span>
-        {reducirVoltage(voltageIn, voltageOut)}
-        <Button
-          className="px-2"
-          onClick={() => copyToClipboard(reducirVoltage(voltageIn, voltageOut))}
-        >
-          <div className="hover:text-blue-600 dark:text-white" aria-label={'copy text'} />
-        </Button>
-      </h3>
-      <h3>
-        <span className="font-bold">
-          Impedancia del bobinado secundario: <br />
-        </span>
-        {elevarVoltage(voltageIn, voltageOut)}
-        <button
-          className="px-2"
-          onClick={() => copyToClipboard(elevarVoltage(voltageIn, voltageOut))}
-        >
-          <div className="hover:text-blue-600 dark:text-white" size={24} aria-label={'copy text'} />
-        </button>
-      </h3>
-      <h3>
-        <span className="font-bold">
-          Factor de acoplamiento sencillo: <br />
-        </span>
-        {couplingFactorSimple(voltageIn, voltageOut)}
-        <button
-          className="px-2"
-          onClick={() => copyToClipboard(couplingFactorSimple(voltageIn, voltageOut))}
-        >
-          <div className="hover:text-blue-600 dark:text-white" aria-label={'copy text'}>
-            copu
-          </div>
-        </button>
-      </h3>
-      <h3>
-        <span className="font-bold">
-          Factor de acoplamiento: <br />
-        </span>
-        {couplingFactor(voltageIn, voltageOut)}
-      </h3>
+      <Card>
+        <CardContent>
+          <CardTitle>Resultados</CardTitle>
+          <span className="font-bold">
+            Impedancia del bobinado primario: <br />
+          </span>
+          {reducirVoltage(voltageIn, voltageOut)}
+          <Button
+            className="px-2"
+            onClick={() => copyToClipboard(reducirVoltage(voltageIn, voltageOut))}
+          >
+            <div className="hover:text-blue-600 dark:text-white" aria-label={'copy text'} />
+          </Button>
+          <span className="font-bold">
+            Impedancia del bobinado secundario: <br />
+          </span>
+          {elevarVoltage(voltageIn, voltageOut)}
+          <button
+            className="px-2"
+            onClick={() => copyToClipboard(elevarVoltage(voltageIn, voltageOut))}
+          >
+            <div
+              className="hover:text-blue-600 dark:text-white"
+              size={24}
+              aria-label={'copy text'}
+            />
+          </button>
+          <span className="font-bold">
+            Factor de acoplamiento sencillo: <br />
+          </span>
+          {couplingFactorSimple(voltageIn, voltageOut)}
+          <button
+            className="px-2"
+            onClick={() => copyToClipboard(couplingFactorSimple(voltageIn, voltageOut))}
+          >
+            <div className="hover:text-blue-600 dark:text-white" aria-label={'copy text'}>
+              copu
+            </div>
+          </button>
+          ={' '}
+          <h3>
+            <span className="font-bold">
+              Factor de acoplamiento: <br />
+            </span>
+            {couplingFactor(voltageIn, voltageOut)}
+          </h3>
+        </CardContent>
+      </Card>
     </div>
   )
 }
