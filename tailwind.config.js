d1939b3432a3ea11443f075d8e77545d7c9a4ef4
Emmanuel Hernandez <e805177@gmail.com>
2024-03-09 16:34:43 -0600

    Se instala shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,13 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       lineHeight: {
         11: '2.75rem',
@@ -65,7 +72,25 @@ module.exports = {
           },
         },
       }),
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animate'),
+  ],
 }
