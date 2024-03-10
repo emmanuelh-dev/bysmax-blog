eca392b5949bcdd4d20129c3aaa94b7d4d31b8ab
Emmanuel Hernandez <e805177@gmail.com>
2024-03-09 18:08:35 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'jsx-a11y/heading-has-content': 'off',
   },
 }
