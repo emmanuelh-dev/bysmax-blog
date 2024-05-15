c086cb39eba4d255b75a4b3b134226607a42a891
Emmanuel Hernandez <e805177@gmail.com>
2024-05-15 11:05:58 -0600

    Update Services with images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,51 @@
+'use client'
+import { useState } from 'react'
 import NextImage, { ImageProps } from 'next/image'
+import { IoClose } from 'react-icons/io5'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.keyCode === 27) {
+      closeModal()
+    }
+  }
+
+  return (
+    <div>
+      <button className="relative cursor-pointer" onClick={openModal}>
+        <NextImage {...rest} className="h-auto w-full" />
+      </button>
+
+      {/* Modal para mostrar la imagen a pantalla completa */}
+      {isModalOpen && (
+        <dialog
+          className="bg- fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-75"
+          open={isModalOpen}
+          onKeyDown={handleKeyDown}
+        >
+          <button
+            className="absolute right-4 top-4 z-50 text-xl text-white"
+            onClick={closeModal}
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+          >
+            <IoClose className="size-20" />
+          </button>
+          <NextImage {...rest} className="max-h-[40rem] w-full object-contain" />
+        </dialog>
+      )}
+    </div>
+  )
+}
 
 export default Image
