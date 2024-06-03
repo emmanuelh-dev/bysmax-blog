d82584da4aa2409959df1eb9678232a7d0d09fe7
Emmanuel Hernandez <e805177@gmail.com>
2024-06-03 01:24:03 -0600

    update GoogleAds.tsx and PostLayout.tsx

--- layouts/GoogleAds.tsx
+++ layouts/GoogleAds.tsx
@@ -1,25 +1,13 @@
 'use client'
 import React, { useEffect } from 'react'
 
-export default function GoogleAds() {
+const AdComponent: React.FC = () => {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const adsbygoogle = (window as any).adsbygoogle
-    if (adsbygoogle && adsbygoogle.push) {
-      adsbygoogle.push({})
-    }
+    ;((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({})
   }, [])
 
-  return (
-    <div>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'inline-block', width: 220, height: 728 }}
-        data-ad-client="ca-pub-3646138644530578"
-        data-ad-slot="6731750998"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
-  )
+  return <></>
 }
+
+export default AdComponent
