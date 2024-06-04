55ba1be2beb468f19e1e130d43ada3b3d5df4a48
emmanuelh-dev <e805177@gmail.com>
2024-06-04 00:54:23 -0600

    update tag-data.json and next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cse.google.com pagead2.googlesyndication.com https://va.vercel-scripts.com https://tpc.googlesyndication.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cse.google.com pagead2.googlesyndication.com va.vercel-scripts.com tpc.googlesyndication.com googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
