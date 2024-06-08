d0594c6f0d43ae19e1c2d013c60c82b5d9d0aa31
Emmanuel Hernandez <e805177@gmail.com>
2024-06-08 03:21:31 -0600

    Change the software download

--- components/SoftwareDownload.tsx
+++ components/SoftwareDownload.tsx
@@ -14,33 +14,48 @@ export default function SoftwareDownload({ mainImage, downloadLink, title, descr
 
   return (
     <main className="mb-10">
-      <div className="mx-auto grid items-end gap-4 md:grid-cols-2">
-        <div>
-          <h2 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-            {title}
-          </h2>
-          <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
-            {description}
-          </p>
-          <div className="mt-6 flex flex-col gap-2 min-[400px]:flex-row">
-            <a
-              href={downloadLink}
-              className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-              target="blank"
-            >
-              Descargar
-            </a>
+      <section className="w-full">
+        <div className="container grid gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
+          <Image
+            src={gallery[0].src || '/placeholder.svg'}
+            width="1920"
+            height="1920"
+            alt="Hero"
+            className="mx-auto aspect-square overflow-hidden rounded-xl object-cover sm:w-full"
+          />
+          <div className="flex flex-col justify-center space-y-4">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                {title}
+              </h2>
+              <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">
+                {description}
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              <a
+                href={downloadLink}
+                className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+              >
+                Descargar
+              </a>
+              {/* <Link
+                href="/#"
+                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                prefetch={false}
+              >
+                Más Información
+              </Link> */}
+            </div>
           </div>
         </div>
-        <Image
-          src={mainImage ?? '/placeholder.svg'}
-          width="1270"
-          height="300"
-          alt={description}
-          className="mx-auto aspect-[4/2] overflow-hidden rounded object-cover"
-        />
-      </div>
-      <Gallery gallery={gallery} />
+      </section>
+      <section>
+        <div>
+          <h2 className="text-5xl">Capturas de pantalla</h2>
+        </div>
+        <Gallery gallery={gallery} />
+      </section>
     </main>
   )
 }
