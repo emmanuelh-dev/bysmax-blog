79bd3cbc9f10482a0ef29dead10ece7031aebaca
Emmanuel Hernandez <e805177@gmail.com>
2024-06-13 18:03:24 -0600

    fix prettier

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-6xl xl:px-0 overflow-x-hidden">
+    <section className="mx-auto max-w-3xl overflow-x-hidden px-4 sm:px-6 xl:max-w-6xl xl:px-0">
       {children}
       <GoogleAds />
     </section>
