080166bc071885c04fde4cc83d8c9effbfcb87dd
Emmanuel Hernandez <e805177@gmail.com>
2024-06-15 01:55:23 -0600

    change microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import { Metadata } from 'next'
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Script from 'next/script'
+import Metrics from './metrics'
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
@@ -87,18 +88,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3646138644530578"
         crossOrigin="anonymous"
       ></Script>
-      <Script
-        id="clarity"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ms0781vg0u");
-          `,
-        }}
-      />
       <body className="overflow-x-hidden bg-white text-black antialiased dark:bg-black dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
@@ -114,6 +103,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </SectionContainer>
         </ThemeProviders>
+        <Metrics />
       </body>
     </html>
   )
