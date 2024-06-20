09b227240de3e651c16e1e9213b16382b8bf2819
Emmanuel Hernandez <e805177@gmail.com>
2024-06-20 01:03:34 -0600

    prettier

--- app/Recommended.tsx
+++ app/Recommended.tsx
@@ -3,9 +3,7 @@ import Main from './Main'
 import { allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 
-
 export default function Recommended({ tags, title }) {
-
   const filteredPosts = allBlogs.filter((post) => tags.some((tag) => post.tags.includes(tag)))
 
   const posts = allCoreContent(filteredPosts)
