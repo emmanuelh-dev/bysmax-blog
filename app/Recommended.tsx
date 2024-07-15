70dbb62d9d540703dffe54f8dd31db8e32583d78
Emmanuel Hernandez <e805177@gmail.com>
2024-07-15 13:52:20 -0700

    fix site metadata

--- app/Recommended.tsx
+++ app/Recommended.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from './Main'
 import { allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
-import { SectionContainerWithAds } from '@/components/SectionContainer'
+import SectionContainer from '@/components/SectionContainer'
 
 export default function Recommended({ tags, title }) {
   const filteredPosts = allBlogs.filter((post) => tags.some((tag) => post.tags.includes(tag)))
@@ -10,8 +10,8 @@ export default function Recommended({ tags, title }) {
   const posts = allCoreContent(filteredPosts)
 
   return (
-    <SectionContainerWithAds>
+    <SectionContainer>
       <Main posts={posts} title={title} />
-    </SectionContainerWithAds>
+    </SectionContainer>
   )
 }
