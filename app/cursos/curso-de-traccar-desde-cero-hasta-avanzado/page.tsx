8a0abb91701d96a8707ddd0252dc361c509232ee
Emmanuel Hernandez <e805177@gmail.com>
2024-07-24 09:14:10 -0700

    update curso

--- app/cursos/curso-de-traccar-desde-cero-hasta-avanzado/page.tsx
+++ app/cursos/curso-de-traccar-desde-cero-hasta-avanzado/page.tsx
@@ -4,5 +4,12 @@ export const metadata = genPageMetadata({
   title: 'Curso Gratuito de Traccar desde Cero hasta Avanzado en español',
   description:
     'Este curso en español te guiará desde la configuración inicial de tu propio servidor de rastreo hasta la gestión avanzada de dispositivos y la personalización de la plataforma.',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Curso Gratuito de Traccar desde Cero hasta Avanzado en español',
+    description:
+      'Este curso en español te guiará desde la configuración inicial de tu propio servidor de rastreo hasta la gestión avanzada de dispositivos y la personalización de la plataforma',
+    images: '/static/images/curso-traccar.jpg',
+  },
 })
 export { Home as default } from '@/components/traccar/Home'
