074c722d429a6f6c13b9a4dd044bff68fb80ca9a
Emmanuel Hernandez <e805177@gmail.com>
2024-07-24 07:43:38 -0700

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
-
 export default function Page() {
-  return <div>Hola</div>
+  return (
+    <div className="space-y-6">
+      <h1 className="py-4 text-6xl">Contacto</h1>
+      <div>
+        <h2 className="py-3 text-4xl">Mándanos un correo</h2>
+        <a href="mailto:info@bysmax.com">info@bysmax.com</a>
+      </div>
+      <div>
+        <h2 className="py-3 text-4xl">Mándanos un WhatsApp</h2>
+        <a href="https://api.whatsapp.com/send/?phone=8126060795">8111913676</a>
+      </div>
+    </div>
+  )
 }
