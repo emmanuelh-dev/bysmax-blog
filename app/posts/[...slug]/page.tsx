063c1d3e09ca581f8350d591b054ce3788b42457
Emmanuel Hernandez <e805177@gmail.com>
2024-07-24 18:00:09 -0700

    add sitemetadata

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,6 +1,62 @@
 import 'css/prism.css'
 import 'katex/dist/katex.css'
 import Layout from '@/layouts/WordPressLayout'
+import siteMetadata from '@/data/siteMetadata'
+import { Metadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string[] }
+}): Promise<Metadata | undefined> {
+  const slug = decodeURI(params.slug.join('/'))
+
+  const response = await fetch('https://cdn.bysmax.com/index.php?graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+      query PostQuery($slug: String!) {
+        postBy(slug: $slug) {
+          id
+          title
+          content
+        }
+      }
+    `,
+      variables: {
+        slug: slug,
+      },
+    }),
+  })
+
+  const { data } = await response.json()
+  const imageList = []
+  return {
+    title: data.postBy.title,
+    description: data.postBy.summary,
+    openGraph: {
+      title: data.postBy.title,
+      description: siteMetadata.description,
+      siteName: siteMetadata.title,
+      locale: 'es_MX',
+      type: 'article',
+      publishedTime: data.postBy.date,
+      modifiedTime: data.postBy.date,
+      url: './',
+      images: imageList,
+      authors: [siteMetadata.author],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: data.postBy.title,
+      description: data.postBy.summary,
+      images: imageList,
+    },
+  }
+}
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const slug = decodeURI(params.slug.join('/'))
