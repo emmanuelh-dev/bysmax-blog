a9890b63fcadc5495ec2e42416096090ba630bf5
Emmanuel Hernandez <e805177@gmail.com>
2024-08-03 14:55:36 -0700

    fix tags

--- app/[locale]/tags/[tag]/page.tsx
+++ app/[locale]/tags/[tag]/page.tsx
@@ -1,41 +1,48 @@
-import { slug } from 'github-slugger'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayout'
-import { allBlogs } from 'contentlayer/generated'
-import tagData from 'app/[locale]/tag-data.json'
-import { genPageMetadata } from '@/app/[locale]/seo'
 import { Metadata } from 'next'
+import Tag from '@/components/tag'
+import tagData from 'app/[locale]/tag-data.json'
+import { genPageMetadata } from 'app/[locale]/seo'
+import { createTranslation } from '../../i18n/server'
+import { LocaleTypes } from '../../i18n/settings'
 
-export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
-  const tag = decodeURI(params.tag)
+type TagsProps = {
+  params: { locale: LocaleTypes }
+}
+
+export async function generateMetadata({ params: { locale } }: TagsProps): Promise<Metadata> {
+  const { t } = await createTranslation(locale, 'SEO')
   return genPageMetadata({
-    title: tag,
-    description: `${siteMetadata.title} ${tag} tagged content`,
-    alternates: {
-      canonical: './',
-      types: {
-        'application/rss+xml': `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`,
-      },
-    },
+    title: 'Tags',
+    description: t('tags'),
+    params: { locale: locale },
   })
 }
 
-export const generateStaticParams = async () => {
-  const tagCounts = tagData as Record<string, number>
+export default function Page({ params: { locale } }: TagsProps) {
+  const tagCounts = tagData[locale]
   const tagKeys = Object.keys(tagCounts)
-  const paths = tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
-  }))
-  return paths
-}
+  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
 
-export default function TagPage({ params }: { params: { tag: string } }) {
-  const tag = decodeURI(params.tag)
-  // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
-  const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
+  return (
+    <>
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-heading-400 dark:text-heading-400 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+            Tags
+          </h1>
+        </div>
+        <div className="flex max-w-lg flex-wrap">
+          {tagKeys.length === 0 && 'No tags found.'}
+          {sortedTags.map((tag) => (
+            <div key={tag} className="mb-2 mr-5 mt-2">
+              <Tag text={tag} />
+              <span className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300">
+                {`(${tagCounts[tag]})`}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
-  return <ListLayout posts={filteredPosts} title={title} />
 }
