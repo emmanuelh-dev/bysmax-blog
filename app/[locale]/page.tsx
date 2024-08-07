4dfa0fdcd40b7363c0f631d8e0af86e7bac918ba
Emmanuel Hernandez <e805177@gmail.com>
2024-08-06 21:59:40 -0700

    add home translation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -26,18 +26,15 @@ export default async function Page({ params: { locale } }) {
   const posts = allCoreContent(sortedPosts)
   const filteredPosts = posts.filter((p) => p.language === locale)
 
-  const { t } = await createTranslation(locale, 'common')
+  const { t } = await createTranslation(locale, 'home')
   return (
     <div>
       <section className="flex w-full items-center justify-between py-12 max-sm:flex-col-reverse max-sm:font-semibold">
         <div className="space-y-4">
           <h1 className="text-6xl font-semibold leading-tight md:max-w-[40rem] md:text-7xl">
-            Electrónica para Estudiantes, Recursos y Más
+            {t('title')}
           </h1>
-          <p className="pt-2 text-lg text-gray-500 dark:text-gray-400">
-            Accede a tutoriales detallados, guías prácticas y proyectos innovadores que te ayudarán
-            a dominar el mundo de la electrónica.
-          </p>
+          <p className="pt-2 text-lg text-gray-500 dark:text-gray-400">{t('description')}</p>
           <div className="flex gap-4">
             <Link href="/blog" className={buttonVariants({ variant: 'default' })}>
               Blog y Tutoriales
@@ -58,7 +55,7 @@ export default async function Page({ params: { locale } }) {
       <section className="py-12">
         <div className=" divide-gray-200 dark:divide-gray-700">
           <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {t('blog')}
+            Blog
           </h2>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {/*siteMetadata.description*/}
