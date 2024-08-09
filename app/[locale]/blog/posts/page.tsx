8dc7f2b29c2c7d3afa227a0a3e51a18138f826c3
Emmanuel Hernandez <e805177@gmail.com>
2024-08-08 22:20:39 -0700

    Update page.tsx

--- app/[locale]/blog/posts/page.tsx
+++ app/[locale]/blog/posts/page.tsx
@@ -3,17 +3,45 @@ import dynamic from 'next/dynamic'
 import SuspencePosts from '@/layouts/components/SuspencePosts'
 import siteMetadata from '@/data/siteMetadata'
 import { genPageMetadata } from '../../seo'
+import { getPostBySlug } from '@/components/util/wpGraphQL'
+import { graphqlToBlog } from '@/components/util/blogFormatter'
+import { maintitle } from '@/data/localeMetadata'
 
 const Posts = dynamic(() => import('@/components/ShowPosts'), {
   loading: () => <SuspencePosts />,
   ssr: false,
 })
 
-export async function generateMetadata({ params: { locale } }) {
-  return genPageMetadata({
-    title: 'Posts',
-    params: { locale: locale },
-  })
+export async function generateMetadata({ params: { slug, locale } }) {
+  const dslug = decodeURI(slug.join('/'))
+  const post = await getPostBySlug(dslug)
+  const blog = graphqlToBlog({ post })
+
+  if (!post) {
+    null
+  }
+  return {
+    title: blog.title,
+    description: blog.summary,
+    openGraph: {
+      title: blog.title,
+      description: blog.summary,
+      siteName: maintitle[locale],
+      locale: blog.language,
+      type: 'article',
+      publishedTime: blog.date,
+      modifiedTime: blog.date,
+      url: './',
+      images: siteMetadata.socialBanner,
+      authors: [siteMetadata.author],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: blog.title,
+      description: blog.summary,
+      images: siteMetadata.socialBanner,
+    },
+  }
 }
 
 export default async function Page({ params: { locale } }) {
