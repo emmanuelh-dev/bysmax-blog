368988c529fe3a3c2e4ae17ffb4c5a6ebc19f133
Emmanuel Hernandez <e805177@gmail.com>
2025-01-09 11:13:17 -0600

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = async () => {
   const footerLinks = [...nav]
 
   return (
-    <div className='container mx-auto'>
+    <div className="container mx-auto">
       <footer className="py-20">
         {/* <div className="flex flex-col py-6 lg:flex-row lg:justify-between">
           <div>
