283007e0830974d4a9907aad64e89250d1911c97
Emmanuel Hernandez <e805177@gmail.com>
2025-01-10 22:17:01 -0600

    fix

--- layouts/CursoLayout.tsx
+++ layouts/CursoLayout.tsx
@@ -35,7 +35,7 @@ export function CursoLayout({
         <SectionContainerWithAds>
           {/* Left Sidebar - Fixed with scroll */}
           <div className="border-r border-neutral-300 dark:border-gray-800">
-            <aside className="overflow-y-auto lg:h-[90vh] sticky top-10">
+            <aside className="sticky top-10 overflow-y-auto lg:h-[90vh]">
               <nav className="space-y-6 p-4">
                 <div className="space-y-2">
                   {sidebar.map((item, index) => (
