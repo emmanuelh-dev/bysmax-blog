cd938f05881c64c16db554c209d116dae0c7de01
Emmanuel Hernandez <e805177@gmail.com>
2025-05-05 11:32:28 -0600

    fix prettier

--- app/[locale]/(gps)/gps-cuts/layout.tsx
+++ app/[locale]/(gps)/gps-cuts/layout.tsx
@@ -2,7 +2,7 @@ export default function GPSCutsLayout({ children }: { children: React.ReactNode 
   return (
     <section>
       <div className="mb-8">
-        <nav className="breadcrumbs text-sm px-4">
+        <nav className="breadcrumbs px-4 text-sm">
           <ul className="flex items-center space-x-2 text-gray-500">
             <li>
               <a href="/" className="hover:text-gray-900">
