bc75373e012851c5625e4357dab93004e4b1e4f7
Emmanuel Hernandez <e805177@gmail.com>
2025-05-15 23:40:00 -0600

    feat: implement lazy loading for ShowPosts component and optimize post fetching

--- components/ShowPosts.tsx
+++ components/ShowPosts.tsx
@@ -1,17 +1,22 @@
-import siteMetadata from '@/data/siteMetadata'
-import Image from 'next/image'
-import Link from 'next/link'
-import { formatDate } from 'pliny/utils/formatDate'
 import React from 'react'
 import { getPosts } from './util/wpGraphQL'
 import ListLayout from '@/layouts/ListLayout'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
+import { POSTS_PER_PAGE } from '@/data/postsPerPage'
 
 export default async function ShowPosts({ locale }) {
-  const posts = await getPosts({ locale })
-  const blogs = allCoreContent(sortPosts(allBlogs))
+  // Cargar posts en paralelo
+  const [wpPosts, blogs] = await Promise.all([
+    getPosts({ locale }),
+    Promise.resolve(allCoreContent(sortPosts(allBlogs))),
+  ])
+
+  // Filtrar y ordenar posts
   const filteredPosts = blogs.filter((post) => post.language === locale)
-  const allPosts = [...posts, ...filteredPosts]
-  return <ListLayout params={{ locale: locale }} posts={allPosts} title="all" />
+  const allPosts = [...wpPosts, ...filteredPosts].sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime()
+  })
+
+  return <ListLayout params={{ locale }} posts={allPosts} title="all" />
 }
